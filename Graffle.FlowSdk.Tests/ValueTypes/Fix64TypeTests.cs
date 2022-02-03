1e2622cc5f5bc33c84ac907570b6bd63ca5068b5
Dakota Kincer <DKincer@Me.com>
2022-02-03 16:41:31 -0500

    Added Fix64

---
+++ Graffle.FlowSdk.Tests/ValueTypes/Fix64TypeTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Graffle.FlowSdk.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Graffle.FlowSdk.Tests.ValueTypes
+{
+    [TestClass]
+    public class Fix64TypeTypeTests
+    {
+        [TestMethod]
+        public void Given_Valid_Cadence_Json_Create_ValueType()
+        {
+            var cadenceJsonString = @"{""type"":""Fix64"",""value"":""100""}";
+            var flowValueType = Fix64Type.FromJson(cadenceJsonString);
+            Assert.AreEqual(flowValueType.Type, "Fix64");
+            Assert.AreEqual(flowValueType.Data, 100m);
+        }
+
+        [TestMethod]
+        public void Given_Value_Create_Fix64Type()
+        {
+            var flowValueType = new Fix64Type(100m);
+            Assert.AreEqual(flowValueType.Type, "Fix64");
+            Assert.AreEqual(flowValueType.Data, 100m);
+        }
+
+        [TestMethod]
+        public void Given_Fix64_Convert_To_Cadence_Json()
+        {
+            var flowValueType = new Fix64Type(100m);
+            var cadenceValue = flowValueType.AsJsonCadenceDataFormat();
+            var cadenceExpected = @"{""type"":""Fix64"",""value"":""100""}";
+            Assert.AreEqual(cadenceExpected,cadenceValue);
+        }
+    }
+}
