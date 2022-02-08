8f89d815c76d8240935788ad5bfcee41692a1d34
Ben Adelson <ben.adelson@graffle.io>
2022-02-08 15:53:29 -0500

    fix whitespace

--- Graffle.FlowSdk.Tests/AccountTests/ConfigAccountTests.cs
+++ Graffle.FlowSdk.Tests/AccountTests/ConfigAccountTests.cs
@@ -13,17 +13,19 @@ namespace Graffle.FlowSdk.Tests.AccountTests
     public class ConfigAccountTests
     {
         private FlowClient flowClient { get; }
-        
-        public ConfigAccountTests(){
+
+        public ConfigAccountTests()
+        {
             this.flowClient = FlowClient.Create(TestHelpers.EmulatorURI);
-        }        
+        }
 
         [TestMethod]
-        public async Task Given_Valid_AccountName_When_GetAccountFromConfigAsync_Then_Load_And_Parse_From_FlowJson(){
+        public async Task Given_Valid_AccountName_When_GetAccountFromConfigAsync_Then_Load_And_Parse_From_FlowJson()
+        {
             var account = await flowClient.GetAccountFromConfigAsync("emulator-account");
             Assert.AreEqual("f8d6e0586b0a20c7", account.Address.ByteStringToHex());
             Assert.AreEqual((UInt32)2, account.Keys[0].SignAlgo);
-            Assert.AreEqual((UInt32)3, account.Keys[0].HashAlgo);            
+            Assert.AreEqual((UInt32)3, account.Keys[0].HashAlgo);
         }
     }
 }
