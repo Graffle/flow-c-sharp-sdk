cbdb2855cfeacade6a9d69dbac776825ebc35b41
Ben Adelson <ben.adelson@graffle.io>
2022-10-04 10:20:49 -0400

    escape newline characters in json

--- Graffle.FlowSdk.Tests/ValueTypes/StringTypeTests.cs
+++ Graffle.FlowSdk.Tests/ValueTypes/StringTypeTests.cs
@@ -32,5 +32,22 @@ namespace Graffle.FlowSdk.Tests.ValueTypes
             var cadenceExpected = @"{""type"":""String"",""value"":""Test""}";
             Assert.AreEqual(cadenceExpected, cadenceValue);
         }
+
+        [TestMethod]
+        public void FromJson_NewLine_ReturnsStringType()
+        {
+            var json = "{\"type\":\"String\",\"value\":\"Appearance: The xG Reward for players with game time in a fixture.\n\nGet xG Rewards for your football achievements.\nBuild your collection - your story.\nUnlock xG experiences.\n\nhttps://linktr.ee/xgstudios\"}";
+
+            var res = StringType.FromJson(json);
+
+            Assert.IsNotNull(res);
+
+            var data = res.Data;
+            Assert.IsNotNull(data);
+            Assert.IsTrue(data.Length > 0);
+
+            Assert.IsFalse(data.Contains("\\n"));
+            Assert.IsTrue(data.Contains("\n"));
+        }
     }
 }
--- Graffle.FlowSdk/Types/StringType.cs
+++ Graffle.FlowSdk/Types/StringType.cs
@@ -1,17 +1,23 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace Graffle.FlowSdk.Types
 {
     public class StringType : FlowValueType<string>
     {
+        private static Regex NEWLINE_REGEX = new Regex("\n", RegexOptions.Compiled);
+        private const string ESCAPED_NEWLINE = "\\n";
+
         public StringType(string value) : base(value)
         {
         }
 
         public static StringType FromJson(string json)
         {
-            var parsedJson = JsonDocument.Parse(json);
+            var escaped = NEWLINE_REGEX.Replace(json, ESCAPED_NEWLINE);
+
+            var parsedJson = JsonDocument.Parse(escaped);
             var value = parsedJson.RootElement.GetProperty("value");
             return new StringType(value.GetString());
         }
